7f5b63d4d3afaad5f36666b9cefb6e288801a0e3
Caleb Meyer <Kiaulen@gmail.com>
2014-09-25 21:24:38 -0500

    Basic mod, no content. Added mcmod.info

---
+++ src/main/java/com/calebmeyer/bettercrafting/BetterCraftingTables.java
@@ -0,0 +1,29 @@
+package com.calebmeyer.bettercrafting;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "BetterCraftingTables", name = "Better Crafting Tables", version = "1.7.10-0.1")
+public class BetterCraftingTables {
+
+    // An instance of this mod for forge to use
+    @Mod.Instance("BetterCraftingTables")
+    public static BetterCraftingTables instance;
+
+    @Mod.EventHandler
+    public void preInitialization(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void initialization(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInitialization(FMLPostInitializationEvent event) {
+
+    }
+}
